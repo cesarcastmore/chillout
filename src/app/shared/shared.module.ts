9d06bd1da45d26b39d758808279beafe96e25805
ccastillo_tegik <ccastillo@tegik.com>
2019-05-17 15:53:57 -0500

    Modulo interno y modulos externos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+@NgModule({
+  declarations: [CardComponent, HeaderComponent, BodyComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    AccordionModule.forRoot()
+  ],
+  providers: [],
+  exports: [CardComponent, HeaderComponent, BodyComponent, FooterComponent]
+})
+export class SharedModule {}
